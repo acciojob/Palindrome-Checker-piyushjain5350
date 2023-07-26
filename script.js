ec37afad4d3d168b47ec52adcb2bf5a4bd85150b
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-07-26 22:50:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,18 @@
 
 function palindrome(str){
 
+	var s=0;
+	var e=str.length()-1;
+
+	str=str.toLowerCase();
+
+	while(s<=e){
+		if(str.charAt(s)!==str.charAt(e)){
+			return false;
+		}
+		s++;
+		e--;
+	}
+	return true;
 }
 module.exports = palindrome
