3679e032ecbc9fad70b8a27ca1d73b23915ade44
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-07-26 22:55:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,20 @@
 
 function palindrome(str){
 
-	var s=0;
-	var e=str.length()-1;
+	// var s=0;
+	// var e=str.length()-1;
 
-	str=str.toLowerCase();
+	// str=str.toLowerCase();
 
-	while(s<=e){
-		if(str.charAt(s)!==str.charAt(e)){
-			return false;
-		}
-		s++;
-		e--;
-	}
-	return true;
+	// while(s<=e){
+	// 	if(str.charAt(s)!==str.charAt(e)){
+	// 		return false;
+	// 	}
+	// 	s++;
+	// 	e--;
+	// }
+	// return true;
+	s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+	return s === s.split('').reverse().join('');
 }
 module.exports = palindrome
